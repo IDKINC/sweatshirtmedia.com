346351917ed12086a312b8a6013ef3f6c9902aba
Jack Harner <jack@harnerdesigns.com>
2020-02-19 20:35:23 -0700

    team link

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,122 +1,155 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import styled from "styled-components"
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import styled from "styled-components";
 
-import logo from '../img/logo.svg'
+import logo from "../img/logo.svg";
 
+import Layout from "../components/Layout";
+import TeamCard from "../components/team/TeamCard";
+import ProjectCard from "../components/project/ProjectCard";
 
-import Layout from '../components/Layout'
-import TeamCard from "../components/team/TeamCard"
-import ProjectCard from "../components/project/ProjectCard"
+import Grid from "../components/layout/grid";
 
-import Grid from "../components/layout/grid"
+import { breakpoints } from "../components/breakpoints";
 
-import { breakpoints } from "../components/breakpoints"
+import SocialIcons from "../components/SocialIcons";
 
-import SocialIcons from '../components/SocialIcons'
+import VideoBanner from "../components/VideoBanner";
 
-import VideoBanner from '../components/VideoBanner'
+import { Container } from "../components/atoms/Container";
 
-import { Container } from '../components/atoms/Container'
+import BannerCover from "../img/videobg.jpg";
 
-import BannerCover from "../img/videobg.jpg"
+import SweatshirtIcon from "../img/sweatshirt-icon.svg";
+import Button from "../components/atoms/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import SweatshirtIcon from "../img/sweatshirt-icon.svg"
-import Button from '../components/atoms/Button'
+export const IndexPageTemplate = ({ image, team, projects, storyTeller }) => (
+  <div>
+    <Container
+      style={{
+        padding: "1rem",
+        minHeight: "100vh",
+        backgroundImage: `url(${BannerCover})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        alignItems: "center",
+        justifyContent: "center",
+        backfaceVisibility: "visible",
+        position: "relative"
+      }}
+    >
+      <VideoBanner />
 
+      <SweatshirtIconStyled src={SweatshirtIcon} alt="Sweatshirt" />
+      <div
+        style={{
+          zIndex: "999",
+          position: "relative",
+          width: "80%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column"
+        }}
+      >
+        <img
+          src={logo}
+          alt="Sweatshirt"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <BannerNav>
+          <Button to="/portfolio" label="See Our Work" white />
+          <Button to="/contact" label="Get In Touch &raquo;" />
+        </BannerNav>
+        <SocialIcons />
+      </div>
+    </Container>
 
+    <Container>
+      <h1>
+        <Link to="/portfolio">We Are Makers.</Link>
+      </h1>
+      <Grid col={3}>
+        {projects.map(({ node: project }, i) => (
+          <ProjectCard project={project} />
+          // <ProjectCard project={project} featured={i === 0}/>
+        ))}
 
-export const IndexPageTemplate = ({
-  image,
-  team,
-  projects,
-  storyTeller
-}) => (
-    <div>
+        <Button
+          to="/portfolio"
+          label="See The Rest Of Our Work &raquo;"
+          size="large"
+          style={{ gridColumn: "1 / -1" }}
+        />
+      </Grid>
+    </Container>
 
-      <Container style={{
-        padding: "1rem", minHeight: "100vh", backgroundImage: `url(${BannerCover})`, backgroundSize: "cover", backgroundPosition: "center", backgroundAttachment: "fixed", alignItems: "center", justifyContent: "center", backfaceVisibility: "visible", position: 'relative',
-      }} >
+    <Container
+      style={{
+        minHeight: "80vh",
+        backgroundImage: `url(${
+          !!storyTeller.childImageSharp
+            ? storyTeller.childImageSharp.fluid.src
+            : storyTeller
+        })`,
+        backgroundPosition: "center center",
+        backgroundAttachment: "fixed"
+      }}
+    >
+      <h1
+        style={{
+          color: "#fff",
+          width: "100%",
+          textShadow:
+            "1px 1px 0 var(--mainColor), 2px 2px 0 var(--darkerColor), 3px 3px 0 var(--darkerColor), 4px 4px 0 var(--darkerColor), 5px 5px 0 var(--darkerColor)"
+        }}
+      >
+        We Are Storytellers.
+      </h1>
+      <Grid col={3} style={{ background: "transparent", width: "80%" }}>
+        <SkillsCard>
+          <h4>Brand Identity</h4>
+        </SkillsCard>
+        <SkillsCard>
+          <h4>Video Production</h4>
+        </SkillsCard>
+        <SkillsCard>
+          <h4>Photography</h4>
+        </SkillsCard>
+        <SkillsCard>
+          <h4>Art Direction</h4>
+        </SkillsCard>
+        <SkillsCard>
+          <h4>Web Development</h4>
+        </SkillsCard>
+        <SkillsCard>
+          <h4>Marketing Strategy</h4>
+        </SkillsCard>
+      </Grid>
+    </Container>
 
-        <VideoBanner />
-
-
-        <SweatshirtIconStyled src={SweatshirtIcon} alt="Sweatshirt" />
-        <div style={{ zIndex: "999", position: "relative", width: "80%", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
-          <img src={logo} alt="Sweatshirt" style={{ width: '100%', height: 'auto' }} />
-          <BannerNav>
-
-            <Button to="/portfolio" label="See Our Work" white />
-            <Button to="/contact" label="Get In Touch &raquo;" />
-
-          </BannerNav>
-          <SocialIcons />
-        </div>
-
-
-
-      </Container>
-
-      <Container>
-        <h1><Link to="/portfolio">We Are Makers.</Link></h1>
-        <Grid col={3}>
-          {projects.map(({ node: project }, i) => (
-            <ProjectCard project={project} />
-            // <ProjectCard project={project} featured={i === 0}/>
-          ))}
-
-          <Button to="/portfolio" label="See The Rest Of Our Work &raquo;" size="large" style={{ gridColumn: "1 / -1" }} />
-        </Grid>
-
-
-
-      </Container>
-
-      <Container style={{
-        minHeight: '80vh', backgroundImage: `url(${
-          !!storyTeller.childImageSharp ? storyTeller.childImageSharp.fluid.src : storyTeller
-          })`, backgroundPosition: "center center", backgroundAttachment: "fixed"
-      }}>
-        <h1 style={{ color: "#fff", width: "100%", textShadow: "1px 1px 0 var(--mainColor), 2px 2px 0 var(--darkerColor), 3px 3px 0 var(--darkerColor), 4px 4px 0 var(--darkerColor), 5px 5px 0 var(--darkerColor)" }}>We Are Storytellers.</h1>
-        <Grid col={3} style={{ background: "transparent", width: "80%" }}>
-          <SkillsCard>
-            <h4>Brand Identity</h4>
-          </SkillsCard>
-          <SkillsCard>
-            <h4>Video Production</h4>
-          </SkillsCard>
-          <SkillsCard>
-            <h4>Photography</h4>
-          </SkillsCard>
-          <SkillsCard>
-            <h4>Art Direction</h4>
-          </SkillsCard>
-          <SkillsCard>
-            <h4>Web Development</h4>
-          </SkillsCard>
-          <SkillsCard>
-            <h4>Marketing Strategy</h4>
-          </SkillsCard>
-        </Grid>
-
-      </Container>
-
-
-      <Container>
-        <h1><Link to="/team">Get To Know Us.</Link></h1>
-        <Grid col={4}>
-          {team.map(({ node: member }) => (
-            <TeamCard person={member} />
-          ))}
-        </Grid>
-      </Container>
-    </div>
-  )
+    <Container>
+      <h1>
+        <Link to="/team">Get To Know Us.</Link>
+      </h1>
+      <Grid col={4}>
+        {team.map(({ node: member }) => (
+          <TeamCard person={member} />
+        ))}
+        <MeetTheTeam to="/team">
+          Meet The Team <FontAwesomeIcon icon="arrow-right" />
+        </MeetTheTeam>
+      </Grid>
+    </Container>
+  </div>
+);
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.index
-  const { team, projects, storyTeller } = data
+  const { frontmatter } = data.index;
+  const { team, projects, storyTeller } = data;
 
   return (
     <Layout noHeader>
@@ -127,167 +160,211 @@ const IndexPage = ({ data }) => {
         storyTeller={storyTeller}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+      frontmatter: PropTypes.object
+    })
+  })
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
-  index: markdownRemark(frontmatter: {templateKey: {eq: "index-page"}}) {
-    frontmatter {
-      title
-      image {
-        childImageSharp {
-          fluid(maxWidth: 2048, quality: 100) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-      portfolioHeader
-    }
-  }
-  storyTeller: file(relativePath: {eq: "what-the-hex-dark.png"}) {
-    childImageSharp {
-      fluid(maxWidth: 2400) {
-        ...GatsbyImageSharpFluid
-      }
-    }
-  }
-  team: allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "team-member"}}}, limit: 1000, sort: {fields: fields___weight, order: DESC}) {
-    edges {
-      node {
-        id
-        frontmatter {
-          name
-          jobTitle
-          featuredImage {
-            childImageSharp {
-              fluid(maxWidth: 600, maxHeight: 800, cropFocus: CENTER) {
-                ...GatsbyImageSharpFluid
-                src
-              }
+    index: markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
             }
           }
         }
-        fields {
-          teamID
-          slug
+        portfolioHeader
+      }
+    }
+    storyTeller: file(relativePath: { eq: "what-the-hex-dark.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 2400) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
-  }
-  projects: allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "project"}}}, limit: 6, sort: {fields: fields___weight, order: DESC}) {
-    edges {
-      node {
-        frontmatter {
-          title
-          
-          featuredImage {
-            childImageSharp {
-              resize(width: 600, height: 600, cropFocus: CENTER) {
-                src
+    team: allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "team-member" } } }
+      limit: 1000
+      sort: { fields: fields___weight, order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            name
+            jobTitle
+            featuredImage {
+              childImageSharp {
+                fluid(maxWidth: 600, maxHeight: 800, cropFocus: CENTER) {
+                  ...GatsbyImageSharpFluid
+                  src
+                }
               }
             }
           }
+          fields {
+            teamID
+            slug
+          }
         }
-        fields {
-          type
-          slug
-          clients
+      }
+    }
+    projects: allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "project" } } }
+      limit: 6
+      sort: { fields: fields___weight, order: DESC }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+
+            featuredImage {
+              childImageSharp {
+                resize(width: 600, height: 600, cropFocus: CENTER) {
+                  src
+                }
+              }
+            }
+          }
+          fields {
+            type
+            slug
+            clients
+          }
         }
       }
     }
   }
-}
-
-`
-
-
-
+`;
 
 const SkillsCard = styled.div`
-background: #fff;
-padding: 1rem;
-transition: 100ms;
-box-shadow: var(--boxShadow);
-border-radius: var(--borderRadius);
+  background: #fff;
+  padding: 1rem;
+  transition: 100ms;
+  box-shadow: var(--boxShadow);
+  border-radius: var(--borderRadius);
 
-display: flex;
-align-items: center;
-justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-h4{
-  margin: 0;
-  text-align: center;
+  h4 {
+    margin: 0;
+    text-align: center;
 
-
-  font-size: 0.8em;
-
-  @media ${breakpoints.laptop} {
-    font-size: inherit;
-
-  } 
-}
-
-&:hover{
-  background: var(--mainColor);
-  color: #fff;
-}
-`
-
-const SweatshirtIconStyled = styled.img`
-
-opacity:0.5; 
-width: 15%; 
-height: auto; 
-position:absolute; 
-bottom:1rem; 
-  right:1rem;
-  z-index: 999999;
-
-@media ${breakpoints.laptop} {
-  bottom:3em; 
-  right:3em;
-  width: 10%; 
-
-  }
-
-`
-
-
-const BannerNav = styled.nav`
-
-width: 100%;
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
-margin: 2rem 0;
-color: #fff;
-
-a{
-  margin-bottom: 1em;
-}
+    font-size: 0.8em;
 
     @media ${breakpoints.laptop} {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      grid-gap: 1rem;
-
-      width: 50vw;
-
+      font-size: inherit;
     }
+  }
 
-`
+  &:hover {
+    background: var(--mainColor);
+    color: #fff;
+  }
+`;
 
+const SweatshirtIconStyled = styled.img`
+  opacity: 0.5;
+  width: 15%;
+  height: auto;
+  position: absolute;
+  bottom: 1rem;
+  right: 1rem;
+  z-index: 999999;
 
+  @media ${breakpoints.laptop} {
+    bottom: 3em;
+    right: 3em;
+    width: 10%;
+  }
+`;
 
+const BannerNav = styled.nav`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 2rem 0;
+  color: #fff;
+
+  a {
+    margin-bottom: 1em;
+  }
+
+  @media ${breakpoints.laptop} {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1rem;
+
+    width: 50vw;
+  }
+`;
+
+const MeetTheTeam = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  position: relative;
+  overflow: hidden;
+  border-radius: var(--borderRadius);
+  z-index: 1;
+  transition: 300ms;
+
+  @media ${breakpoints.laptop} {
+    flex-direction: column;
+
+    font-size: 2rem;
+    svg {
+      width: 4rem;
+    }
+  }
+
+&:before{
+  position: absolute;
+  width: 0%;
+  height: 100%;
+  left: 0;
+  background: var(--mainColor);
+  content: '';
+  z-index: -1;
+  transition: 300ms;
+}
+
+  &:hover{
+    color: #fff;
+
+    &:before{
+
+    width: 100%;
+    }
+  }
+
+  &:active{
+
+    color: var(--darkerColor);
+
+    &:before{
+      left: unset;
+      right: 0;
+      width: 0;
+    }
+  }
+`;
