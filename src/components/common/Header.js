a58a03f4f3bdfb3f8829487c31668cbca5b87eff
Jack Harner <jack@harnerdesigns.com>
2020-05-16 18:03:55 -0600

    menu animation

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,25 +3,37 @@ import styled, { keyframes } from "styled-components";
 
 import logo from "../../img/logo.svg";
 import icon from "../../img/sweatshirt-icon.svg";
+import outline from "../../img/sweatshirt-icon--outline.svg";
 import { breakpoints } from "../breakpoints";
 import { Link } from "gatsby";
+
+import Helmet from "react-helmet";
 
 const Header = ({ whiteIcon }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <Icon
-        src={icon}
-        alt="Sweatshirt Icon"
+      <IconWrapper
         onClick={(e) => {
           e.preventDefault();
           setIsOpen(!isOpen);
         }}
-        className={
-          (isOpen ? "is-open" : "") + " " + (whiteIcon ? "white-icon" : "")
-        }
-      />
+        className={isOpen ? "is-open" : ""}
+      >
+        <Icon
+          src={icon}
+          alt="Sweatshirt Icon"
+          onClick={(e) => {
+            e.preventDefault();
+            setIsOpen(!isOpen);
+          }}
+          className={
+            (isOpen ? "is-open" : "") + " " + (whiteIcon ? "white-icon" : "")
+          }
+        />
+        <span>{isOpen ? "hide" : "menu"}</span>
+      </IconWrapper>
       <StyledHeader className={isOpen ? "is-open" : ""}>
         <HeaderWrapper>
           <LogoWrapper to="/" title="Logo">
@@ -57,6 +69,17 @@ const slideIn = keyframes`
 
   to {
     width: 100%;
+  }
+`;
+
+const flip = keyframes`
+  0% {
+    transform: rotateY(0deg);
+  
+  }
+
+  100% {
+    transform: rotateY(180deg);
   }
 `;
 
@@ -111,42 +134,75 @@ const NavBG = styled.div`
 `;
 
 const Icon = styled.img`
-  width: 50vw;
-  /* filter: invert(1); */
   max-height: 100% !important;
   display: block;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: fixed;
   top: 0;
   left: 0%;
 
-  z-index: 99999999999999999999999999;
-  transition: 0;
+  z-index: 99999999999999999999999999; /* It's OVER 9000 */
+
+  transform: rotateY(0deg);
+
+  transition: transform 300ms;
+
   background: rgba(51, 51, 51, 0.3);
 
   clip-path: polygon(0 0, 100% 0, 100% 85%, 85% 100%, 0 100%);
 
-  height: 5em;
-  width: 5em;
-  object-fit: contain;
-  padding: 0.75em;
+  height: 5rem;
+  width: 5rem;
+  flex-direction: column;
 
-  &.white-icon {
-    /* filter: invert(0); */
+  padding: 0.75em;
+  letter-spacing: 6px;
+
+  color: #fff;
+  font-weight: 100;
+  cursor: pointer;
+  transition: 300ms;
+
+  span, ${Icon}{
+    
+    transform: rotateY(0deg);
+
+transition: transform 600ms;
+  }
+
+  span{transition-duration: 800ms;  }
+
+  &:hover {
+    background: rgba(51, 51, 51, 0.6);
   }
 
   &.is-open {
-    /* filter: invert(0); */
-    transition: 300ms;
-
     background: var(--mainColor);
+    transform: rotateY(180deg);
+
+    span, ${Icon}{
+      transform: rotateY(-180deg);
+
+    }
+
+    &:hover {
+      background: var(--darkerColor);
+    }
+
+    ${Icon} {
+    }
   }
 
   @media ${breakpoints.laptop} {
-    height: 7em;
-    width: 7em;
-    object-fit: contain;
-    margin: 1em;
+    height: 9.5rem;
+    width: 7.5rem;
   }
+
 `;
 
 const Nav = styled.nav`
@@ -189,6 +245,7 @@ const StyledHeader = styled.header`
   top: 0;
   width: 100%;
   transition: height 300ms, background 300ms;
+  will-change: transform;
 
   height: 0;
 
@@ -226,6 +283,7 @@ const StyledHeader = styled.header`
 
     @media ${breakpoints.laptop} {
       height: 9em;
+      min-height: 0;
     }
   }
 `;
