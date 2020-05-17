f054a5424f3808525ce48a33104cfdd350a1512e
Jack Harner <jack@harnerdesigns.com>
2020-05-17 16:32:56 -0600

    Contact Page

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -5,10 +5,18 @@ import ContactForm from "../components/contact/contactForm";
 import { breakpoints } from "../components/breakpoints";
 
 import styled from "styled-components";
-import bgImg from "../img/city-bg.jpg";
+
 import SocialIcons from "../components/SocialIcons";
 import SEO from "../components/seo";
 import Clock from "../components/Clock";
+
+
+import atlanta from "../img/city-bg.jpg";
+import la from "../img/la.jpg";
+import albuquerque from "../img/albuquerque.jpg";
+import austin from "../img/austin.jpg";
+import boston from "../img/boston.jpg";
+import paris from "../img/paris.jpg";
 
 function encode(data) {
   return Object.keys(data)
@@ -16,33 +24,45 @@ function encode(data) {
     .join("&");
 }
 
+const timeZones = [
+  { offset: "America/Los_Angeles", city: "Los Angeles", img: la },
+  { offset: "America/Denver", city: "Albuquerque", img: albuquerque },
+  { offset: "America/Chicago", city: "Austin", img: austin },
+  { offset: "America/New_York", city: "Atlanta", img: atlanta },
+  { offset: "America/New_York", city: "Boston", img: boston },
+  { offset: "Europe/Paris", city: "Paris", img: paris}
+];
+
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
+
+    let selectedCity = timeZones[Math.floor(Math.random()*5)];
     this.state = {
       isValidated: false,
       pageContent: { ...this.props.data.markdownRemark.frontmatter },
+      selectedCity: selectedCity.city,
+      selectedBG: selectedCity.img,
+
     };
     console.log(this.state);
   }
+
+  changeBG = (city, bg) => {
+    this.setState({selectedCity: city, selectedBG: bg});
+  };
   render() {
     let content = this.state.pageContent;
-    const timeZones = [
-      { offset: "America/Los_Angeles", city: "Los Angeles" },
-      { offset: "America/Denver", city: "Albuquerque" },
-      { offset: "America/Chicago", city: "Houston" },
-      { offset: "America/New_York", city: "New York" },
-    ];
 
-    const clocks = timeZones.map(({ offset, city }, i) => {
+    const clocks = timeZones.map(({ offset, city, img }, i) => {
       console.log({ index: i, offset: offset });
-      return <Clock key={i} tzOffset={offset} city={city} />;
+      return <Clock key={i} tzOffset={offset} city={city} onClick={()=>this.changeBG(city, img)} selected={(this.state.selectedCity === city)} />;
     });
     return (
-      <Layout cta={false}>
+      <Layout cta={false} noFooter>
         <SEO title="Contact" />
 
-        <ContactContainer>
+        <ContactContainer bg={this.state.selectedBG}>
           <ClockContainer>
             <Wherever>Wherever You Need Us:</Wherever>
             {clocks}
@@ -58,8 +78,10 @@ export default class Index extends React.Component {
                 submit: content.submitLabel,
               }}
             />
+
+          <SocialIcons style={{ margin: "auto", color: "#333" }} />
+
           </ContactContent>
-          <SocialIcons style={{ marginBottom: "auto" }} />
         </ContactContainer>
       </Layout>
     );
@@ -84,17 +106,22 @@ export const ContactPageQuery = graphql`
 
 const ContactContainer = styled.section`
   width: 100%;
-  display: flex;
-  background: var(--mainColor) url(${bgImg}) top center no-repeat;
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: flex-start;
+  background: var(--mainColor) center center no-repeat;
+  background-image: url(${(props) => props.bg || "none"});
   background-size: cover;
+  background-attachment: fixed;
   overflow: hidden;
   flex-direction: column;
   padding-top: 7em;
 
   @media ${breakpoints.laptop} {
-    flex-direction: column;
-    min-height: 85vh;
+    min-height: 100vh;
     padding-top: 0;
+  grid-template-columns: 1fr 5fr 5fr;
+
   }
 `;
 
@@ -106,14 +133,23 @@ const ClockContainer = styled.div`
   margin-top: auto;
   width: 80%;
   margin: 1em auto 1em;
+  transition: 300ms;
 
   @media ${breakpoints.laptop} {
-    margin: auto auto 1em;
+    margin: auto;
 
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr;
+    background: linear-gradient(to right,  rgba(51,51,51,0.4), rgba(51,51,51,0));
+
     color: #fff;
+    padding: 1em;
 
-    width: 50%;
+    width: 100%;
+
+    &:hover{
+    background: linear-gradient(to right,  rgba(51,51,51,0.6), rgba(51,51,51,0));
+
+    }
   }
 `;
 
@@ -125,13 +161,13 @@ const Wherever = styled.span`
 
   @media ${breakpoints.laptop} {
 
-    grid-column: 1;
+    grid-column: 1 / -1;
 
   }
 `;
 
 const ContactContent = styled.div`
-  background: #fff;
+  /* background: #fff; */
 
   padding: 1em;
 
@@ -147,10 +183,16 @@ const ContactContent = styled.div`
     width: 95%;
   }
 
+  h1{
+    color: #fff;
+    margin-top: auto;
+  }
+
   @media ${breakpoints.laptop} {
-    width: 50%;
-    margin: 1rem auto;
-    background: #fff;
+    width: 100%;
+    height: 100%;
+    margin: auto;
+    /* background: #fff; */
     z-index: 1;
     position: relative;
   }
